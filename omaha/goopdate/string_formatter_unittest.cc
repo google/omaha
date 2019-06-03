dabb33fe842d138ee88bdc1b0451e4f9b363b527
Sorin Jianu <sorin@chromium.org>
2019-06-03 13:56:28 -0700

    Fix broken test StringFormatterTest.FormatMessageTest

--- omaha/goopdate/string_formatter_unittest.cc
+++ omaha/goopdate/string_formatter_unittest.cc
@@ -77,7 +77,7 @@ TEST_F(StringFormatterTest, FormatMessageTest) {
   StringFormatter formatter_de(_T("de"));
   EXPECT_HRESULT_SUCCEEDED(
       formatter_de.FormatMessage(&format_result, IDS_CLOSE));
-  EXPECT_STREQ(_T("Schlie�en"), format_result);   // NOLINT
+  EXPECT_STREQ(_T("Schlie\u00dfen"), format_result);   // NOLINT
 
   StringFormatter formatter_fr(_T("fr"));
   EXPECT_HRESULT_SUCCEEDED(
