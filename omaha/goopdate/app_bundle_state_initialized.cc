21c9d5ed97d1ab5c1f96e799e981e7c45f579e87
Sorin Jianu <sorin@chromium.org>
2021-07-28 16:10:09 -0700

    Revert sending the current version "pv" for over-installs
    This CL is causing some rule breakage in the update configs, because we are sending the current version "pv" in update checks with a '-' in front of the `current_version`. Reverting this CL for now, we will revisit with a better change if there is enough demand for the feature.

--- omaha/goopdate/app_bundle_state_initialized.cc
+++ omaha/goopdate/app_bundle_state_initialized.cc
@@ -106,10 +106,9 @@ HRESULT AppBundleStateInitialized::CreateApp(AppBundle* app_bundle,
     return hr;
   }
 
-  // When overinstalling, we want the pv and install age for the existing
-  // install, so explicitly get it here. These are the only values read from the
-  // registry for installs.
-  AppManager::Instance()->ReadFirstInstallAppVersion(local_app.get());
+  // When overinstalling, we want the install age for the existing install, so
+  // explicitly get it here. These are the only values read from the registry
+  // for installs.
   AppManager::Instance()->ReadAppInstallTimeDiff(local_app.get());
   AppManager::Instance()->ReadDayOfInstall(local_app.get());
 
